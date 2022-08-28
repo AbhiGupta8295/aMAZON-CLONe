f18dcf1ef64a8a805fa5e0821f3bafc662d05c8e
Abhishek Gupta <91731524+AbhiGupta8295@users.noreply.github.com>
2022-08-28 16:44:19 +0530

    Add files via upload
    This is a MERN stack based project with different components, css folders.

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/home";
+function App() {
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+return (
+  <div className="app">
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  </div>
+);
+}
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA2Xad9CdNsK4bKq2cMQML5csQYyJCq43o",
+    authDomain: "clone-4f69b.firebaseapp.com",
+    projectId: "clone-4f69b",
+    storageBucket: "clone-4f69b.appspot.com",
+    messagingSenderId: "1047535566537",
+    appId: "1:1047535566537:web:5b87003ec88e2465ef1718",
+    measurementId: "G-VYC7J6GXJJ"
+  };
